b88c0146bf45fe3f0b34e5c8e343224abc156b12
Ragdata <ragdata@users.noreply.github.com>
2023-04-14 02:35:42 +1000

    feat(project): initial commit

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# ==================================================================
+# install.sh
+# ==================================================================
+# LabHacker's Docker Swarm Module Installer
+#
+# File:         install.sh
+# Author:       Ragdata
+# Date:         13/04/2023
+# License:      MIT License
+# Copyright:    Copyright © 2023 Darren (Ragdata) Poulton
+# ==================================================================
+# PREFLIGHT
+# ==================================================================
